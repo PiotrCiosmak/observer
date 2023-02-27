e525c5edb97a1d136c73d526e83859548ac3be9e
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-27 17:52:37 +0100

    main is now empty

--- src/main/java/com/ciosmak/Main.java
+++ src/main/java/com/ciosmak/Main.java
@@ -1,5 +1,9 @@
-package com.ciosmak;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+package com.ciosmak;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
     }
 }
