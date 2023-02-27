6f636d3bdb55610b2445cb3fa6705e1983d675e0
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-27 18:43:43 +0100

    creating radar and adding two subscribers, after that radar starts work

--- src/main/java/com/ciosmak/Main.java
+++ src/main/java/com/ciosmak/Main.java
@@ -1,9 +1,17 @@
 package com.ciosmak;
 
+import com.ciosmak.client.EnemyShip;
+import com.ciosmak.client.FriendlyShip;
+import com.ciosmak.radar.Radar;
+
 public class Main
 {
     public static void main(String[] args)
     {
+        Radar radar = new Radar();
+        radar.addSubscriber(new EnemyShip());
+        radar.addSubscriber(new FriendlyShip());
 
+        radar.startWork();
     }
 }
