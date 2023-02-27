e683ea3b6a4bb0c1dd6c41e2e9226d417d5bf066
PiotrCiosmak <piotrciosmak2001@gmail.com>
2023-02-27 18:28:55 +0100

    Radar can notify all subscribers about a friendly or an enemy ship

---
+++ src/main/java/com/ciosmak/radar/Radar.java
@@ -0,0 +1,80 @@
+package com.ciosmak.radar;
+
+import com.ciosmak.client.EnemyShip;
+import com.ciosmak.client.FriendlyShip;
+import com.ciosmak.observer.Observer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Radar
+{
+    private static final double CHANCE_FOR_AN_ENEMY_SHIP = 0.05;
+    private static final double CHANCE_FOR_A_FRIENDLY_SHIP = 0.10;
+    List<Observer> subscribers;
+
+    public Radar()
+    {
+        this.subscribers = new ArrayList<>();
+    }
+
+    public void addSubscriber(Observer observer)
+    {
+        subscribers.add(observer);
+    }
+
+    public void removeSubscriber(Observer observer)
+    {
+        subscribers.remove(subscribers);
+    }
+
+    public void startWork()
+    {
+        notifyAboutNewShip();
+    }
+
+    private void notifyAboutNewShip()
+    {
+        notifyAboutNewFriendlyShip();
+        notifyAboutNewEnemyShip();
+    }
+
+    private void notifyAboutNewFriendlyShip()
+    {
+        if (newFriendlyShipAppeared())
+        {
+            for (Observer subscriber : subscribers)
+            {
+                if (subscriber instanceof FriendlyShip)
+                {
+                    subscriber.update();
+                }
+            }
+        }
+    }
+
+    private void notifyAboutNewEnemyShip()
+    {
+        if (newEnemyShipAppeared())
+        {
+            for (Observer subscriber : subscribers)
+            {
+                if (subscriber instanceof EnemyShip)
+                {
+                    subscriber.update();
+                }
+            }
+        }
+    }
+
+    private boolean newFriendlyShipAppeared()
+    {
+        return new Random().nextDouble() < CHANCE_FOR_AN_ENEMY_SHIP;
+    }
+
+    private boolean newEnemyShipAppeared()
+    {
+        return new Random().nextDouble() < CHANCE_FOR_A_FRIENDLY_SHIP;
+    }
+}
